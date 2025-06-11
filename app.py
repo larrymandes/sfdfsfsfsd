80417c581c394ce65b1f60c40a362d1f178031ab
larrymandes <156687843+larrymandes@users.noreply.github.com>
2025-06-11 21:46:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -384,4 +384,4 @@ def check_admin_password():
         return jsonify({'success': False})
 
 if __name__ == '__main__':
-    app.run(debug=True, host=0.0.0.0) 
+    app.run(host='0.0.0.0', debug=True) 
